a46df7971f6064b14747b0ab2b6b870108459f85
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-07 18:33:58 +0300

    Add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);
+}
+
+
+void SumElementsArr(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    sum = sum + array[i];
+    Console.WriteLine($"Sum of array elements on odd position: {sum}");
+}
+
+
+Console.Clear();
+Console.Write("Enter number of array elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+SumElementsArr(array);
