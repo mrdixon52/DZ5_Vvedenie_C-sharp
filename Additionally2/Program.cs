2befde57daa01f063553d9ce89507a28ac2de0ef
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-08 00:38:33 +0300

    Add Additionally2

---
+++ Additionally2/Program.cs
@@ -0,0 +1,26 @@
+﻿bool CheckProsto (int a)
+{
+    int i = 2;
+    while (i < a)
+    {
+        if (a % i == 0)
+        return false;
+    i++;
+    }
+    return true;
+}
+
+
+Console.Clear();
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 0;
+for (int j = 2; j < n; j++)
+{
+    if (CheckProsto(j) == true && CheckProsto(n - j) == true) break;
+    min = j + 1;
+    max = n - j - 1;
+}
+Console.WriteLine($"{min} {max}");
+
