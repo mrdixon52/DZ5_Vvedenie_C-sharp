a33b6d31176121400853099b8f7772ed26f0a283
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-07 19:03:58 +0300

    Add Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputDoubleArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+
+
+void DiffMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        max = array[i];
+        if (array[i] < min)
+        min = array[i];
+    }
+    Console.WriteLine($"Max = {max}; Min = {min}");
+    Console.WriteLine($"Difference between max and min: {max - min}");
+}
+
+
+Console.Clear();
+Console.Write("Enter number of array elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputDoubleArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+DiffMaxMin(array);
